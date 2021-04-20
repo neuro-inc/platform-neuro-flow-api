132699c35d7203bb681ae83d4447059b6019eb09
Andrew Svetlov <andrew.svetlov@gmail.com>
2021-04-20 13:10:58 +0300

    zzz

--- tests/integration/test_api.py
+++ tests/integration/test_api.py
@@ -919,7 +919,7 @@ class TestAttemptApi:
         neuro_flow_api: NeuroFlowApiEndpoints,
         regular_user: _User,
         client: aiohttp.ClientSession,
-        bake_factory: Callable[[_User], Awaitable[Project]],
+        bake_factory: Callable[[_User], Awaitable[Bake]],
     ) -> None:
         bake = await bake_factory(regular_user)
         async with client.post(
@@ -946,7 +946,7 @@ class TestAttemptApi:
         neuro_flow_api: NeuroFlowApiEndpoints,
         regular_user: _User,
         client: aiohttp.ClientSession,
-        bake_factory: Callable[[_User], Awaitable[Project]],
+        bake_factory: Callable[[_User], Awaitable[Bake]],
     ) -> None:
         bake = await bake_factory(regular_user)
         async with client.post(
@@ -989,7 +989,7 @@ class TestAttemptApi:
         neuro_flow_api: NeuroFlowApiEndpoints,
         regular_user: _User,
         client: aiohttp.ClientSession,
-        bake_factory: Callable[[_User], Awaitable[Project]],
+        bake_factory: Callable[[_User], Awaitable[Bake]],
     ) -> None:
         bake = await bake_factory(regular_user)
         async with client.post(
@@ -1027,7 +1027,7 @@ class TestAttemptApi:
         neuro_flow_api: NeuroFlowApiEndpoints,
         regular_user: _User,
         client: aiohttp.ClientSession,
-        bake_factory: Callable[[_User], Awaitable[Project]],
+        bake_factory: Callable[[_User], Awaitable[Bake]],
     ) -> None:
         bake = await bake_factory(regular_user)
         async with client.post(
@@ -1069,7 +1069,7 @@ class TestAttemptApi:
         neuro_flow_api: NeuroFlowApiEndpoints,
         regular_user: _User,
         client: aiohttp.ClientSession,
-        bake_factory: Callable[[_User], Awaitable[Project]],
+        bake_factory: Callable[[_User], Awaitable[Bake]],
     ) -> None:
         bake = await bake_factory(regular_user)
         async with client.post(
@@ -1226,43 +1226,45 @@ class TestTaskApi:
                 }
             ]
 
-    # async def test_get(
-    #     self,
-    #     neuro_flow_api: NeuroFlowApiEndpoints,
-    #     regular_user: _User,
-    #     client: aiohttp.ClientSession,
-    #     bake_factory: Callable[[_User], Awaitable[Project]],
-    # ) -> None:
-    #     bake = await bake_factory(regular_user)
-    #     async with client.post(
-    #         url=neuro_flow_api.attempts_url,
-    #         json={
-    #             "bake_id": bake.id,
-    #             "number": 1,
-    #             "result": "pending",
-    #             "configs_meta": self.CONFIGS_META,
-    #         },
-    #         headers=regular_user.headers,
-    #     ) as resp:
-    #         assert resp.status == HTTPCreated.status_code, await resp.text()
-    #         payload = await resp.json()
-    #         attempt_id = payload["id"]
-    #         created_at = payload["created_at"]
+    async def test_get(
+        self,
+        neuro_flow_api: NeuroFlowApiEndpoints,
+        regular_user: _User,
+        client: aiohttp.ClientSession,
+        attempt_factory: Callable[[_User], Awaitable[Attempt]],
+    ) -> None:
+        attempt = await attempt_factory(regular_user)
+        async with client.post(
+            url=neuro_flow_api.tasks_url,
+            json={
+                "yaml_id": "a",
+                "attempt_id": attempt.id,
+                "raw_id": None,
+                "outputs": {},
+                "state": {},
+                "statuses": [],
+            },
+            headers=regular_user.headers,
+        ) as resp:
+            assert resp.status == HTTPCreated.status_code, await resp.text()
+            payload = await resp.json()
+            task_id = payload["id"]
 
-    #     async with client.get(
-    #         url=neuro_flow_api.attempt_url(attempt_id),
-    #         headers=regular_user.headers,
-    #     ) as resp:
-    #         assert resp.status == HTTPOk.status_code, await resp.text()
-    #         payload = await resp.json()
-    #         assert payload == {
-    #             "id": attempt_id,
-    #             "bake_id": bake.id,
-    #             "number": 1,
-    #             "created_at": created_at,
-    #             "result": "pending",
-    #             "configs_meta": self.CONFIGS_META,
-    #         }
+        async with client.get(
+            url=neuro_flow_api.task_url(task_id),
+            headers=regular_user.headers,
+        ) as resp:
+            assert resp.status == HTTPOk.status_code, await resp.text()
+            payload = await resp.json()
+            assert payload == {
+                "id": task_id,
+                "attempt_id": attempt.id,
+                "yaml_id": "a",
+                "raw_id": None,
+                "outputs": {},
+                "state": {},
+                "statuses": [],
+            }
 
     # async def test_get_by_number(
     #     self,
