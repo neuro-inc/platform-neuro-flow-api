25830c6c46e7cc9e36e94c692fb5dc1f3980bd2f
Andrew Svetlov <andrew.svetlov@gmail.com>
2021-05-07 12:56:25 +0300

    Index tasks by attempt id (#24)

---
+++ alembic/versions/d50ee518e13e_index_task_attempt.py
@@ -0,0 +1,28 @@
+"""index_task_attempt
+
+Revision ID: d50ee518e13e
+Revises: 18e02ee52b12
+Create Date: 2021-05-07 10:53:52.764474
+
+"""
+
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = "d50ee518e13e"
+down_revision = "18e02ee52b12"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_index(
+        "tasks_attempt_id",
+        "tasks",
+        ["attempt_id"],
+    )
+
+
+def downgrade():
+    op.drop_index("tasks_attempt_id")
