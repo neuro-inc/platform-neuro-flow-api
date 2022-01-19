09eaf01be43fe5578c44badb66d0e91cb9b9ce88
Andrew Svetlov <andrew.svetlov@gmail.com>
2022-01-19 12:37:11 +0200

    Drop explicit asyncio marker (#213)
    * Drop explicit asyncio marker
    
    Committed via https://github.com/asottile/all-repos
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/integration/test_postgres.py
+++ tests/integration/test_postgres.py
@@ -1,9 +1,7 @@
-import pytest
 import sqlalchemy as sa
 from sqlalchemy.ext.asyncio import AsyncEngine
 
 
-@pytest.mark.asyncio
 async def test_postgres_available(sqalchemy_engine: AsyncEngine) -> None:
     async with sqalchemy_engine.connect() as connection:
         result = await connection.execute(sa.text("SELECT 2 + 2;"))
