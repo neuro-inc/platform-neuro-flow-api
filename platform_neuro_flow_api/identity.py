46aa36047b8c75d6c4a178c17a9fa1202351901f
oleksandr-sandbx <olexandr.kostiuk@sandbx.co>
2025-04-18 12:16:41 +0200

    swagger 3

--- platform_neuro_flow_api/identity.py
+++ platform_neuro_flow_api/identity.py
@@ -23,7 +23,7 @@ async def untrusted_user(request: Request) -> Identity:
     identity = await _get_identity(request)
 
     autz_policy = request.config_dict.get(AUTZ_KEY)
-    name = autz_policy.get_user_name_from_identity(identity)
+    name = autz_policy.get_user_name_from_identity(identity)  # type: ignore[union-attr]
     if name is None:
         raise HTTPUnauthorized()
 
@@ -32,7 +32,7 @@ async def untrusted_user(request: Request) -> Identity:
 
 async def _get_identity(request: Request) -> str:
     identity_policy = request.config_dict.get(IDENTITY_KEY)
-    identity = await identity_policy.identify(request)
+    identity = await identity_policy.identify(request)  # type: ignore[union-attr]
     if identity is None:
         raise HTTPUnauthorized()
     return identity
